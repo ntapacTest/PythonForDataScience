5bf4ca6e84934a999f3815d50183156625a2163c
Vic <alvic2004@ukr.net>
2019-11-20 18:27:11 +0200

    First commit

---
+++ 20191120/lesson01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+import keyword
+
+print(keyword.kwlist)
+
+x=int(input('Enter the number:'))
+
+if x%2==0:
+    print('Четное')
+else:
+    print('Нечетное')
+
+
